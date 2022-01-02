1b90dbde6ba6e42bbffcd06c0fd73fdf608fb14c
Josh Grancell <josh@joshgrancell.com>
2022-01-02 13:30:28 -0600

    Fixing tests

--- app/app_test.go
+++ app/app_test.go
@@ -1,10 +1,26 @@
 package app
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/jgrancell/metasync/configuration"
+)
 
 func TestSyncSubcommand(t *testing.T) {
+	pwd, _ := os.Getwd()
+
+	os.Chdir("../testdata")
+	c := &configuration.Configuration{
+		ConfigurationPath: ".metasync.yml",
+	}
+	c.Load()
+	os.Chdir(pwd)
+
 	a := &App{
-		Subcommand: "sync",
+		Configuration: c,
+		Dryrun:        true,
+		Subcommand:    "sync",
 	}
 
 	if a.Run() != 0 {
